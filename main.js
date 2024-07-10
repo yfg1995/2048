17dbb1c5c6379ca5ae9a349bd7cd3b8361aa0962
yfg1995 <andonovic995@gmail.com>
2024-07-10 22:31:15 +0200

    generateGrid fn

--- main.js
+++ main.js
@@ -6,17 +6,18 @@ const getRandomCell = () => {
   return Math.floor(Math.random() * gridSize) + 1;
 };
 
-const grid = document.querySelector('.grid');
+// const grid = document.querySelector('.grid');
 
-for (let i = 0; i < gridSize; i++) {
-  for (let j = 0; j < gridSize; j++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    row.appendChild(cell);
-  }
+// for (let i = 0; i < gridSize; i++) {
+//   for (let j = 0; j < gridSize; j++) {
+//     const cell = document.createElement('div');
+//     cell.classList.add('cell');
+//     row.appendChild(cell);
+//   }
 
-  grid.appendChild(row);
-}
+//   grid.appendChild(row);
+// }
+
 const grid = [];
 
 const generateGrid = (gridSize) => {
