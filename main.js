35843ff5c4fecc81dc5c1c616a63b2b434e73c71
yfg1995 <andonovic995@gmail.com>
2024-07-14 20:10:35 +0200

    done

--- main.js
+++ main.js
@@ -14,23 +14,24 @@ import {
 } from "./helpers";
 
 class Grid {
+  colors = {
+    0: "#ccc0b3",
+    2: "#eee4da",
+    4: "#ede0c8",
+    8: "#f2b179",
+    16: "#f59563",
+    32: "#f67c5f",
+    64: "#f65e3b",
+    128: "#edcf72",
+    256: "#edcc61",
+    512: "#edc850",
+    1024: "#edc850",
+    2048: "#edc850",
+  };
+
   constructor({ wrapSelector, gridSize }) {
     this.gridWrap = document.querySelector(wrapSelector);
     this.gridSize = gridSize;
-    this.colors = {
-      0: "#ccc0b3",
-      2: "#eee4da",
-      4: "#ede0c8",
-      8: "#f2b179",
-      16: "#f59563",
-      32: "#f67c5f",
-      64: "#f65e3b",
-      128: "#edcf72",
-      256: "#edcc61",
-      512: "#edc850",
-      1024: "#edc850",
-      2048: "#edc850",
-    };
     this.init();
   }
 
